1f4e5ab8010058a2b6500c6e335ce4b7183fab59
CoderTheDuggy <gkollegger5@outlook.com>
2022-07-24 18:20:22 +0200

    Added colored logging!

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import os
 import numpy
 import sys
 
+
+def logOK(msg):
+    print('\033[92m[+] ' + msg + '\033[0m')
+
+
+def logFATAL(msg):
+    print('\033[91m[-] ' + msg + '\033[0m')
+
+
+def logWARNING(msg):
+    print('\033[93m[!] ' + msg + '\033[0m')
+
+
 def getColorChar(rgbAverage):
     if 0 <= rgbAverage < 17:
         return '.'
@@ -37,28 +50,29 @@ def getColorChar(rgbAverage):
 
 
 createFile = True
-print(sys.argv)
-if len(sys.argv)>1:
+if len(sys.argv) > 1:
     if sys.argv[1] == "--no-file":
         createFile = False
+        logWARNING("Save result to file disabled with arg --no-file!")
     else:
-        print("The command line argument " + sys.argv[1] + " is unknown and skipped!" + ("(Along with " + str(len(sys.argv)-2) + " other argument(s)!" if len(sys.argv)>3 else ""))
+        logWARNING("The command line argument " + sys.argv[1] + " is unknown and skipped!" + (
+            "(Along with " + str(len(sys.argv) - 2) + " other argument(s)!" if len(sys.argv) > 3 else ""))
 
-filePath = input('Please enter the path to your image: ')
+filePath = input('\033[92m[+] Please enter the path to your image:\033[0m ')
 if os.path.exists(filePath):
     if os.path.isfile(filePath):
         image = PIL.Image.open(filePath)
-        width = input("Enter width (or nothing/0 to calculate it from height): ")
+        width = input("\033[92m[+] Enter width (or nothing/0 to calculate it from height):\033[0m  ")
         width = width if width else 0
 
-        height = input("Enter height (or nothing/0 to calculate it from width): ")
+        height = input("\033[92m[+] Enter height (or nothing/0 to calculate it from width):\033[0m ")
         height = height if height else 0
 
         if not str(width).isnumeric():
-            print("The width (" + width + ") is not numeric!")
+            logFATAL("The width (" + width + ") is not numeric!")
             exit(0)
         if not str(height).isnumeric():
-            print("The height (" + height + ") is not numeric!")
+            logFATAL("The height (" + height + ") is not numeric!")
             exit(0)
 
         height = int(height)
@@ -67,20 +81,24 @@ if os.path.exists(filePath):
         resultHeight = height
         resultWidth = width
         if height == 0 and width != 0:
-            resultHeight = round(image.height * (image.width / width) if width > image.width else image.height / (image.width / width))
+            resultHeight = round(
+                image.height * (image.width / width) if width > image.width else image.height / (image.width / width))
         elif height != 0 and width == 0:
-            resultWidth = round(image.width * (image.height / height) if height > image.height else image.width / (image.height / height))
+            resultWidth = round(image.width * (image.height / height) if height > image.height else image.width / (
+                        image.height / height))
         elif height == 0 and width == 0:
-            print("Either height or width must be greater than 0!")
+            logFATAL("Either height or width must be greater than 0!")
             exit(0)
 
-        offset = input("Enter a offset for the text (or nothing to calculate the default offset): ")
-        if offset and not str(offset).isnumeric():
-            print("The offset must be numeric!")
-            exit(0)
+        offset = input("\033[92m[+] Enter a offset for the text (or nothing to calculate the default offset):\033[0m ")
+        if offset:
+            if not str(offset).isnumeric():
+                logFATAL("The offset must be numeric!")
+                exit(0)
+            else:
+                offset = " "*int(offset)
 
-        print("Creating text of image!")
-        print("Dimension (HxW): " + str(resultHeight) + "x" + str(resultWidth))
+        logOK("Creating text of image with dimension (HxW) " + str(resultHeight) + "x" + str(resultWidth) + "!")
 
         resizedImage = image.resize((resultHeight, resultWidth))
         result = ""
@@ -89,23 +107,25 @@ if os.path.exists(filePath):
         for y in range(0, resizedImage.height):
             for x in range(0, resizedImage.width):
                 rgb = numpy.array(pixels[x, y])
-                rgbAverage = round((rgb[0] + rgb[1] + rgb[2])/3)
+                rgbAverage = round((rgb[0] + rgb[1] + rgb[2]) / 3)
                 colorChar = getColorChar(rgbAverage)
                 if not offset:
-                    if 100 <= resizedImage.height < 200:
+                    if 100 <= resizedImage.height < 200 or 100 <= resizedImage.width < 200:
                         offset = " "
-                    elif resizedImage.height >= 200:
+                    elif resizedImage.height >= 200 or resizedImage.width >= 200:
                         offset = " "*round(resizedImage.height / 2 / 100)
                 print(colorChar + offset, end="")
                 result = result + colorChar + offset
             print("")
             result = result + "\n"
 
-            if createFile:
-                file = open("./result-" + os.path.basename(filePath) + ".txt", "w")
-                file.write(result)
-                file.close()
+        if createFile:
+            resultFileName = "result-" + os.path.basename(filePath) + ".txt"
+            file = open("./" + resultFileName, "w")
+            file.write(result)
+            file.close()
+            logOK("Saved result to file " + resultFileName + "!")
     else:
-        print("File " + filePath + " isn't a file!")
+        logFATAL("File " + filePath + " isn't a file!")
 else:
-    print("File " + filePath + "doesn't exist!")
+    logFATAL("File " + filePath + "doesn't exist!")
