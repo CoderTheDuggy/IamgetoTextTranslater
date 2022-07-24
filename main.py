e079583432017241c58e2ef1270b527b2590ca46
CoderTheDuggy <gkollegger5@outlook.com>
2022-07-24 15:07:12 +0200

    Added all code to generate text and sav it to file!

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import PIL.Image
 import os
-
-filePath = input('Please enter the path to your image: ')
-print(filePath)
-if os.path.exists(filePath):
-    if os.path.isfile(filePath):
-        image = PIL.Image.OPEN(filePath)
-
-    else:
-        print("File " + filePath + " isn't a file!")
-else:
-    print("File " + filePath + "doesn't exist!")
-
+import numpy
+import sys
 
 def getColorChar(rgbAverage):
     if 0 <= rgbAverage < 17:
@@ -44,3 +34,78 @@ def getColorChar(rgbAverage):
         return '%'
     elif 238 <= rgbAverage <= 255:
         return '#'
+
+
+createFile = True
+print(sys.argv)
+if len(sys.argv)>1:
+    if sys.argv[1] == "--no-file":
+        createFile = False
+    else:
+        print("The command line argument " + sys.argv[1] + " is unknown and skipped!" + ("(Along with " + str(len(sys.argv)-2) + " other argument(s)!" if len(sys.argv)>3 else ""))
+
+filePath = input('Please enter the path to your image: ')
+if os.path.exists(filePath):
+    if os.path.isfile(filePath):
+        image = PIL.Image.open(filePath)
+        width = input("Enter width (or nothing/0 to calculate it from height): ")
+        width = width if width else 0
+
+        height = input("Enter height (or nothing/0 to calculate it from width): ")
+        height = height if height else 0
+
+        if not str(width).isnumeric():
+            print("The width (" + width + ") is not numeric!")
+            exit(0)
+        if not str(height).isnumeric():
+            print("The height (" + height + ") is not numeric!")
+            exit(0)
+
+        height = int(height)
+        width = int(width)
+
+        resultHeight = height
+        resultWidth = width
+        if height == 0 and width != 0:
+            resultHeight = round(image.height * (image.width / width) if width > image.width else image.height / (image.width / width))
+        elif height != 0 and width == 0:
+            resultWidth = round(image.width * (image.height / height) if height > image.height else image.width / (image.height / height))
+        elif height == 0 and width == 0:
+            print("Either height or width must be greater than 0!")
+            exit(0)
+
+        offset = input("Enter a offset for the text (or nothing to calculate the default offset): ")
+        if offset and not str(offset).isnumeric():
+            print("The offset must be numeric!")
+            exit(0)
+
+        print("Creating text of image!")
+        print("Dimension (HxW): " + str(resultHeight) + "x" + str(resultWidth))
+
+        resizedImage = image.resize((resultHeight, resultWidth))
+        result = ""
+        pixels = resizedImage.load()
+
+        for y in range(0, resizedImage.height):
+            for x in range(0, resizedImage.width):
+                rgb = numpy.array(pixels[x, y])
+                rgbAverage = round((rgb[0] + rgb[1] + rgb[2])/3)
+                colorChar = getColorChar(rgbAverage)
+                if not offset:
+                    if 100 <= resizedImage.height < 200:
+                        offset = " "
+                    elif resizedImage.height >= 200:
+                        offset = " "*round(resizedImage.height / 2 / 100)
+                print(colorChar + offset, end="")
+                result = result + colorChar + offset
+            print("")
+            result = result + "\n"
+
+            if createFile:
+                file = open("./result-" + os.path.basename(filePath) + ".txt", "w")
+                file.write(result)
+                file.close()
+    else:
+        print("File " + filePath + " isn't a file!")
+else:
+    print("File " + filePath + "doesn't exist!")
